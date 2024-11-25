64603b4d29dcb61b93e7efa18d5a8d8dfa01a28b
oldmcdonnell <oldmcdonnell@gmail.com>
2024-11-25 15:51:00 -0500

    commit before pull

--- src/app/api/resident-request/route.ts
+++ src/app/api/resident-request/route.ts
@@ -1,29 +1,32 @@
-import { auth } from "@/auth";
+import { authOptions } from "@/auth";
 import { prisma } from "@/lib/prisma";
 import { assignRequestedTimeSlot } from "@/lib/utils/time-slot/time-slot-assigners";
 import { residentRequestValidationSchema } from "@/lib/validation-schemas/validation-schemas";
 import { ResidentReqestApiRequest } from "@/types/resident-request-api-request";
-import { Address } from "@prisma/client";
+import { Address, RequestStatus  } from "@prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
-import { RequestStatus } from "@prisma/client"; 
-
-export const POST = auth(async function POST(req) {
-  if (!req.auth) {
+import { ZodError } from "zod";
+ 
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+  if (!session) {
     return NextResponse.json({ message: "No session" }, { status: 401 });
   }
-
+ 
   let requestBody: ResidentReqestApiRequest;
-
+ 
   try {
     requestBody = await req.json();
     residentRequestValidationSchema.parse(requestBody);
-  } catch (error: any) {
-    return NextResponse.json(
-      { errors: error.issues || "Invalid request body." },
-      { status: 400 }
-    );
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return NextResponse.json({ errors: error.issues }, { status: 400 });
+    } else {
+      return NextResponse.json({ errors: error }, { status: 400 });
+    }
   }
-
+ 
   const {
     name,
     areaCode,
@@ -32,19 +35,19 @@ export const POST = auth(async function POST(req) {
     appointmentDate,
     timeSlot,
   } = requestBody;
-
+ 
   const { startTime, endTime } = assignRequestedTimeSlot(
     appointmentDate,
     timeSlot
   );
-
+ 
   const { address }: { address: Partial<Address> } = googleAddressData;
-
+ 
   // This route is assuming that external users are submitting the request on their own behalf
   // Find the user in the database by e-mail, update their name to the one they entered on the form
   try {
     await prisma.user.update({
-      where: { email: req.auth.user?.email! },
+      where: { email: session.user!.email! },
       data: {
         name,
         phoneNumber: `${areaCode}${phoneNumber}`,
@@ -74,57 +77,61 @@ export const POST = auth(async function POST(req) {
         requests: true,
       },
     });
-  } catch (error: any) {
+  } catch (error) {
     console.error(error);
     return NextResponse.json(
       { message: "Failed to create resident request" },
       { status: 500 }
     );
   }
-
+ 
   return NextResponse.json({ message: "Request received" });
-});
-
-//adding Get Method
-
-export const GET = auth(async function GET(req) {
-  if (!req.auth) {
-    return NextResponse.json({ message: "Unauthorized access" }, { status: 401 });
+}
+ 
+ 
+//adding GET method
+ 
+export async function GET(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+ 
+  if (!session) {
+    return NextResponse.json({ message: "No session" }, { status: 401 });
   }
-
+ 
   const { searchParams } = new URL(req.url);
   const status = searchParams.get("status");
-
+ 
   // Validate the status parameter
   if (!status) {
     return NextResponse.json({ error: "Missing status parameter" }, { status: 400 });
   }
-
+ 
   const validStatuses: RequestStatus[] = [
     RequestStatus.COMPLETED,
     RequestStatus.PENDING,
     RequestStatus.CANCELED,
   ];
-
+ 
+ 
   if (!validStatuses.includes(status as RequestStatus)) {
     return NextResponse.json({ error: "Invalid status parameter" }, { status: 400 });
   }
-
+ 
   try {
     const residentRequests = await prisma.residentRequest.findMany({
-      where: { status: status as RequestStatus },
+      where: { status: status as RequestStatus }, 
       include: {
-        user: true,
-        requestedTimeSlot: true,
+        user: true, 
+        requestedTimeSlot: true, 
       },
     });
-
+ 
     return NextResponse.json(residentRequests);
   } catch (error) {
     console.error("Error fetching resident request:", error);
     return NextResponse.json(
       { error: "Failed to fetch resident request" },
       { status: 500 }
-    );
+    );  
   }
-});
+}
